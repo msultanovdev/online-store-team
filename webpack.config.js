3ce967d2731ca70a618e36b687590f85cf7b1935
Kasyanovskaya Kristina <kristinochkakv@gmail.com>
2022-12-15 23:36:45 +0300

    init: start online-store-team project

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const EslingPlugin = require("eslint-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/index.js"),
+  module: {
+    rules: [
+      { test: /\.svg$/, use: "svg-inline-loader" },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      { test: /\.(js)$/, use: "babel-loader" },
+      { test: /\.ts$/i, use: "ts-loader" },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index_bundle.js",
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./src/index.html"),
+      filename: "index.html",
+    }),
+    new EslingPlugin({ extensions: "ts" }),
+  ],
+};
+module.exports = ({ mode }) => {
+  const isProductionMode = mode === "prod";
+  const envConfig = isProductionMode
+    ? require("./webpack.prod.config")
+    : require("./webpack.dev.config");
+
+  return merge(baseConfig, envConfig);
+};
