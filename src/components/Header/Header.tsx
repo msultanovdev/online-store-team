b6209fc5062bb711050dbededd378da2632ff247
msultanovdev <mukhammadamin.sultanov@gmail.com>
2022-12-23 22:01:00 +0500

    feat: add header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,19 @@
 import React from "react";
+import './Header.css';
 
 const Header = () => {
     return(
-        <div>
-            Header
-        </div>
+        <header className="header">
+            <nav id="main-nav" className="sidebar">
+                <div className="sidebar__links">
+                    <a className="sidebar__links-logo" href="/">Online Store</a>
+                    <a href="/about" data-content="Catalog">Catalog</a>
+                    <a href="!#" data-content="About us">About us</a>
+                    <a href="!#" data-content="Delivery">Delivery</a>
+                </div>
+                <a href="/lorem" className="sidebar__links-basket">&#128722;</a>
+            </nav>
+        </header>
     );
 }
 
