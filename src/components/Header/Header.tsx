0700fa979451ca0589b4eff4af9eca36cf600879
msultanovdev <mukhammadamin.sultanov@gmail.com>
2022-12-28 08:43:29 +0500

    fix: fix header links

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,12 +7,13 @@ const Header = () => {
     <header className="header">
       <nav id="main-nav" className="sidebar">
         <div className="sidebar__links">
-          <Link to="/" className="sidebar__links-logo">
+          <Link to="/catalog" className="sidebar__links-logo">
             Online Store
           </Link>
-          <Link to="/catalog">Catalog</Link>
-          <Link to="/catalog">About us</Link>
-          <Link to="/catalog">Delivery</Link>
+          {/* <Link to="/catalog">Catalog</Link> */}
+        </div>
+        <div className="header__price">
+          <p>Cart Total: 100$</p>
         </div>
         <Link to="/basket" className="sidebar__links-basket">
           &#128722;
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "./Main.css";
+import { Link } from "react-router-dom";
 
 const Main = () => {
   return (
     <div className="main">
-      <div className="main-page"></div>
+      <div className="main-page">
+        <div className="main-page__content">
+          <h1>Welcome to ONLINE-STORE</h1>
+          <p>Please, show our <span><Link to="/catalog">catalog</Link></span></p>
+        </div>
+      </div>
     </div>
   );
 };
