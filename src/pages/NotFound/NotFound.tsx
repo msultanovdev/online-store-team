10adc4e785663fc3e509b5d4dc855c3c0e8db4e6
Kasyanovskaya Kristina <kristinochkakv@gmail.com>
2023-01-06 16:56:40 +0300

    feat: add 404 page styles

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import "./NotFound.css";
 
 const NotFound = () => {
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+  const limit: string | null = searchParams.get("limit");
+  navigate({ pathname: "/NotFound", search: `?limit=${limit}` });
   return (
-    <div>
-      <h1>404 Not Found</h1>
-      <p>Oh no! It looks like the page you are trying to get to is missing!</p>
+    <div className="not__found-page">
+      <div className="not__found-content">
+        <h1>404 Not Found</h1>
+        <p>
+          Oh no! It looks like the page you are trying to get to is missing!
+        </p>
+      </div>
     </div>
   );
 };
