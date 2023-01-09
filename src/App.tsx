c5a15539bf02b0a1eaa1cd0a31cab7d4ea8936ff
msultanovdev <mukhammadamin.sultanov@gmail.com>
2023-01-10 03:31:19 +0500

    refresh master

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ function App() {
 
   const [amount, setAmount] = useState(total.count);
   const [isModalActive, setIsModalActive] = useState(false);
+  console.log('refresh master')
 
   useEffect(() => {
       if(localStorage.length <= 2) {
